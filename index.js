e751e679bf0ebed7d6c33403f6550ae324962932
“CollinABullock” <cbullock@schoolofrock.com>
2023-08-12 14:20:20 -0500

    finished prombt 2

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// Before you begin coding, be sure that you:
-// a. Cleaned up the coffee_data.js file
-// b. Connect the coffee_data.js file to this index.js file.
-
-// Prompt 2:
-// Print an array of all the drinks.
-
-// Prompt 3
-// Print an array of drinks that cost 5 and under.
-
 // Prompt 4
 // Print an array of drinks that are priced at an even number.
 
@@ -21,10 +11,32 @@
 // Print all the seasonal drinks with the words "with imported beans" after the item name.
 // For example: "affogato with imported beans"
 
+// Before you begin coding, be sure that you:
+// a. Cleaned up the coffee_data.js file
+// b. Connect the coffee_data.js file to this index.js file.
+// I'll keep adding all my variables up here so that they're in one place.
+
 const coffeeData = require("./coffee_data_starter");
+
+// Prompt 2:
+// Print an array of all the drinks.
+// I'll use an arrow function, with the const being print name, the parameter being item and then the function returning the value attached to the name key in each object.
+// Then I'll use MAP to console log out an array specifically of items.
+// Console table looks nicer, but I don't think that counts as an array if you wanna be technical.  So I'll just print both.
 
 const printName = (item) => {
   return item.name;
 };
 
 console.log(coffeeData.map(printName));
+console.table(coffeeData.map(printName));
+
+// Prompt 3
+// Print an array of drinks that cost 5 and under.
+
+const print5 = (item) => {
+  return item.price <= 5;
+}
+
+const itemsUnder5 = coffeeData.filter(print5);
+console.log(itemsUnder5);
